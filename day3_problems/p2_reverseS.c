2d6223eb3811dff53e37f3e90f75fdf68beac210
likitharai <likitharai481@gmail.com>
2025-02-19 14:49:48 +0530

    C programming

---
+++ day3_problems/p2_reverseS.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+void reverse(char word[], int length)
+{
+    for (int i=0;i<length/2;i++) {
+        char temp=word[i];
+        word[i]=word[length-i-1];
+        word[length-i-1]=temp;
+    }
+}
+
+void reverseword(char str[]) {
+    char word[100];
+    int i;
+    int wordIndex=0;
+    int length=strlen(str);
+
+    for (i=0;i<=length;i++) {
+        if (str[i]!=' ' && str[i]!='\0') {
+            word[wordIndex++]=str[i];
+        } else {
+            if (wordIndex>0) {
+                word[wordIndex]='\0';
+                reverse(word,wordIndex);
+                for (int k=0;k<wordIndex;k++) {
+                    str[i-wordIndex+k]=word[k];
+                }
+                wordIndex=0;
+            }
+            str[i]=' ';
+        }
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a sentence: ");
+    scanf("%s",str);
+
+    reverseword(str);
+    printf("Reversed Sentence: %s\n",str);
+
+    return 0;
+}
